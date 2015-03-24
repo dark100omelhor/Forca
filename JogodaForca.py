41f9cf8b0195e82009f94ce7e92f13ec85dc709e
dark100omelhor <hh_lmt@hotmail.com>
2015-03-24 10:06:05 -0300

    Ifs
    Já me antecipei para colocar a variável erros para avisar o usuário de
    quantos erros ele já possui.

--- JogodaForca.py
+++ JogodaForca.py
@@ -6,11 +6,23 @@ Created on Tue Mar 24 09:49:46 2015
 """
 
 palavra=input("Digite a palavra aqui:")
-erro=0
+erros=0
 acertos=0
 digitados=[]
 tentativas=[]
 
 while True:
     
-    
+    if erros==1:
+        print("Você tem mais 5 tentativas!")
+    elif erros==2:
+        print("Você tem mais 4 tentativas!")
+    elif erros==3:
+        print("Você tem mais 3 tentativas!")
+    elif erros==4:
+        print("Você tem mais 2 tentativas!")
+    elif erros==5:
+        print("Você tem mais 1 tentativa!")
+    elif erros==6:
+        print("Sinto Muito, você perdeu!")
+        
