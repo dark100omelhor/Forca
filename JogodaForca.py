0c4fee8a81c26b37d30aa5db0c5fa7c958428f9d
dark100omelhor <hh_lmt@hotmail.com>
2015-03-25 18:05:07 -0300

    Mudança total
    descobri que meu programa iria funcionar somente no console, e não no
    turtle como proposto. Tendo isso em vista, mudei ele inteiro, ai vai:

--- JogodaForca.py
+++ JogodaForca.py
@@ -5,26 +5,154 @@ Created on Tue Mar 24 09:49:46 2015
 @author: Homero
 """
 
-palavra=input("Digite a palavra aqui:")
+
+import turtle
+import random
+
+
+
 erros=0
 acertos=[]
-digitados=[]
+erradas=[]
+l=[]
+leitura=[]
 
-while True:
-    tentativas=[]
-    tentativas=input("Digite uma letra:")
+arquivo=open("entradamemo.csv", "r+")
+leitura=arquivo.read(100)
+l = leitura.split(',')
+palavra = random.choice(l)
+print(palavra)
+
+
+window=turtle.Screen()
+window.setup(width=1400, startx=None, starty=None)
+window.title("Jogo da Forca!!!!!!!@@")
+window.bgcolor("purple")
+
+
+trave=turtle.Turtle()
+trave.hideturtle()
+trave.pensize(5)
+trave.speed(2)
+trave.penup()
+trave.setpos(-300,0)
+
+trave.pendown()
+trave.forward(80)
+trave.left(90)
+trave.forward(-40)
+trave.left(90)
+trave.forward(80)
+trave.right(90)
+trave.forward(40)
+trave.right(90)
+trave.forward(40)
+trave.left(90)
+trave.forward(200)
+trave.right(90)
+trave.forward(80)
+trave.right(90)
+trave.forward(20)
+
+
+
+
+
+
+def bonecocabeça():
+     trave=turtle.Turtle()
+     trave.hideturtle()
+     trave.speed(5)
+     trave.pensize(5)
+     trave.penup()
+     trave.setpos(-180,180)
+     trave.left(180)
+     trave.pendown()
+     trave.circle(20)
+     trave.color("black")
+     
+     
+     
+def tronco():
+    trave=turtle.Turtle()
+    trave.hideturtle()
+    trave.speed(5)
+    trave.pensize(5)
+    trave.penup()
+    trave.setpos(-180,140)
+    trave.pendown()
+    trave.right(90)
+    trave.forward(100)
+    trave.color("black")
+    
+def bracoum():
+    trave=turtle.Turtle()
+    trave.hideturtle()
+    trave.speed(5)
+    trave.pensize(5)
+    trave.penup()
+    trave.setpos(-180,120)
+    trave.pendown()
+    trave.right(135)
+    trave.forward(40)
+    trave.color("black")
     
     
-    if erros==1:
-        print("Você tem mais 5 tentativas!")
-    elif erros==2:
-        print("Você tem mais 4 tentativas!")
-    elif erros==3:
-        print("Você tem mais 3 tentativas!")
-    elif erros==4:
-        print("Você tem mais 2 tentativas!")
-    elif erros==5:
-        print("Você tem mais 1 tentativa!")
-    elif erros==6:
-        print("Sinto Muito, você perdeu!")
-    break
+def bracodois():
+    trave=turtle.Turtle()
+    trave.hideturtle()
+    trave.speed(5)
+    trave.pensize(5)
+    trave.penup()
+    trave.setpos(-180,120)
+    trave.pendown()
+    trave.right(400)
+    trave.forward(40)
+    trave.color("black")
+       
+def pernaum():
+    trave=turtle.Turtle()
+    trave.hideturtle()
+    trave.speed(5)
+    trave.pensize(5)
+    trave.penup()
+    trave.setpos(-180,40)
+    trave.pendown()
+    trave.left(220)
+    trave.forward(50)
+    trave.color("black")
+    
+def pernadois():
+    trave=turtle.Turtle()
+    trave.hideturtle()
+    trave.speed(5)
+    trave.pensize(5)
+    trave.penup()
+    trave.setpos(-180,40)
+    trave.pendown()
+    trave.left(310)
+    trave.forward(50)
+    trave.color("black")
+    
+
+
+
+bonecocabeça()
+tronco()
+bracoum()
+bracodois()
+pernaum()
+pernadois()
+
+
+
+window.exitonclick()
+
+
+
+
+
+
+
+
+
